3830c06c7859bc51d3bf7e31ce74850247b8a22a
Sam Wilson <sam@binarycake.ca>
2023-10-23 19:23:36 -0400

    Hopefully fix module resolution for unbundled dependants

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
-import * as actions from "./actions";
-import { EventMap } from "./events";
+import * as actions from "./actions.js";
+import { EventMap } from "./events.js";
 import { createNanoEvents } from "nanoevents";
 
 export type Unsubscribe = () => void;
@@ -8,7 +8,7 @@ export abstract class Glue {
     private readonly emitter = createNanoEvents<EventMap>();
 
     public async next<E extends keyof EventMap>(
-        type: E
+        type: E,
     ): Promise<Parameters<EventMap[E]>[0]> {
         return await new Promise((res, rej) => {
             const unsubscribe = this.on(
@@ -20,14 +20,14 @@ export abstract class Glue {
                     } catch (e: unknown) {
                         rej(e);
                     }
-                }
+                },
             );
         });
     }
 
     public on<E extends keyof EventMap>(
         type: E,
-        listener: EventMap[E]
+        listener: EventMap[E],
     ): Unsubscribe {
         return this.emitter.on(type, listener);
     }
@@ -41,25 +41,27 @@ export abstract class Glue {
 
     public abstract activateChain(action: actions.ActivateChain): Promise<void>;
     public abstract requestAccounts(
-        action: actions.RequestAccounts
+        action: actions.RequestAccounts,
     ): Promise<void>;
     public abstract signMessage(action: actions.SignMessage): Promise<void>;
     public abstract sendTransaction(
-        action: actions.SendTransaction
+        action: actions.SendTransaction,
     ): Promise<void>;
     public abstract signTransaction(
-        action: actions.SignTransaction
+        action: actions.SignTransaction,
     ): Promise<void>;
     public abstract switchEthereumChain(
-        action: actions.SwitchEthereumChain
+        action: actions.SwitchEthereumChain,
     ): Promise<void>;
 
     public addEthereumChain(_action: actions.AddEthereumChain): Promise<void> {
         return Promise.reject(
-            new TypeError("glue does not support addEthereumChain")
+            new TypeError("glue does not support addEthereumChain"),
         );
     }
 }
 
-export * from "./events";
-export * from "./actions";
+export * from "./events.js";
+export type * from "./events.js";
+export * from "./actions.js";
+export type * from "./actions.js";
